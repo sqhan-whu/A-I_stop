e878ffbb7e3799303aee2aa17a351a779616031a
Han Shaoqing <502466541@qq.com>
2021-02-08 15:52:04 +0000

    Create 07.get_23bp_seq.sh

---
+++ 07.get_23bp_seq.sh
@@ -0,0 +1,2 @@
+python get_seq_23.py 3.site.txt /home/weiqi/project/01.weiqi/20200929_inosine/process/index/GRCm38.p6.fa > 3_frequency.txt
+python get_seq_23.py 4.site.txt /home/weiqi/project/01.weiqi/20200929_inosine/process/index/GRCm38.p6.fa > 4_frequency.txt
