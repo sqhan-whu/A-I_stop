57ce0d71f82f7f0c55d69a7395b47180ae6ab753
Han Shaoqing <502466541@qq.com>
2021-02-17 13:41:05 +0000

    Update 08.stat_position_2_base.sh

--- 08.stat_position_2_base.sh
+++ 08.stat_position_2_base.sh
@@ -1,1 +1,5 @@
-python stat_base.py 3.rough.txt 3neg.rough.txt
+python stat_stop_2_base.py 1.rough.txt 1neg.rough.txt > base_2_count1.txt
+python stat_stop_2_base.py 2.rough.txt 2neg.rough.txt > base_2_count2.txt
+python stat_stop_2_base.py 3.rough.txt 3neg.rough.txt > base_2_count3.txt
+python stat_stop_2_base.py 4.rough.txt 4neg.rough.txt > base_2_count4.txt
+
