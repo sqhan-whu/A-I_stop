d66a7d6502b631dafd4acb1d448facb8ca767d92
Han Shaoqing <502466541@qq.com>
2021-02-25 17:21:00 +0800

    Create p2.stat.site.mutation.py

---
+++ python/p2.stat.site.mutation.py
@@ -0,0 +1,21 @@
+import sys
+import collections
+from collections import Counter
+
+def get_AG(site_file):
+	c = collections.defaultdict(lambda :0)
+	with open(site_file) as f:
+		for line in f:
+			line = line.strip().split('\t')
+			if len(line) == 1:
+				continue
+			elif int(line[8]) - int(line[7]) == 1 and float(line[9]) > 26 and line[3] == 'A':
+				row = line[3]+line[4]
+				c[row] +=1
+			else:
+				continue
+	
+	for k,v in c.items():
+		print(k,v,site_file)
+for i in sys.argv:
+	get_AG(i)
