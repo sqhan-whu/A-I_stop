60278c37d9d914cb403c57b1d9d9cb08f80ab6dc
Han Shaoqing <502466541@qq.com>
2021-02-26 21:03:43 +0800

    Update bam_frac.R

--- figure/bam_frac.R
+++ figure/bam_frac.R
@@ -6,14 +6,14 @@ library(splines)
 data <- read.table("site2.txt",head=T)
 color <- c("grey30", "grey50", "#00CC66","#339933")
 ggplot(data = data,aes(x = name/1000000, y = num,group=group)) +
-  geom_point(aes(fill = group,size=5),shape = 21,alpha=.8,size=5)+
+  geom_point(aes(fill = group,size=4),shape = 21,alpha=.8,size=4)+
              #color = "transparent")+
   #在外面家里白色圆框
-  geom_point(aes(size = 5.1), 
+  geom_point(aes(size = 4.1), 
              shape = 21,
              color = "grey50",
              alpha=.8,
-             size=5
+             size=4
              #fill = "transparent",
   ) +
   scale_fill_manual(values = color,name = "")+
@@ -21,7 +21,7 @@ ggplot(data = data,aes(x = name/1000000, y = num,group=group)) +
 	guides(size=guide_legend(label.position = "top",
 	                        override.aes = list(color=color[2],stroke=.9,fill=NA)),
 	       fill = guide_legend(label.position = "top",
-	                           override.aes = list(size=5)))+
+	                           override.aes = list(size=4)))+
   labs(x = "Uniquely mapped reads(× 106)",
        y = "The number of I sites detected")+
       #title = "Base Charts in R Exercise 01: <span style='color:#D20F26'>Point Charts</span>",
@@ -43,6 +43,19 @@ ggplot(data = data,aes(x = name/1000000, y = num,group=group)) +
 #geom_smooth(aes(col=group),method = 'lm',size = 1, se=FALSE,colour = "black",linetype=2,alpha=.4)+
  geom_smooth(method="loess",se=F,size=.5,linetype=2,aes(group=group),col="grey60")+
  scale_x_continuous(limits = c(0,50))
+
+ggsave("bam_frac.pdf",width=5.5,height=4)
+
+
+fit_F1 <- lm(name~num, data = subset(data, group == 'F-2-1'))
+fit_F2 <- lm(name~num, data = subset(data, group == 'F-2-2'))
+fit_F3 <- lm(name~num, data = subset(data, group == 'F-2-3'))
+fit_F4 <- lm(name~num, data = subset(data, group == 'F-2-4'))
+
+R2_F1 <- summary(fit_F1)$adj.r.squared 
+R2_F2 <- summary(fit_F2)$adj.r.squared 
+R2_F3 <- summary(fit_F3)$adj.r.squared 
+R2_F4 <- summary(fit_F4)$adj.r.squared 
 
 ggsave("bam_frac.pdf",width=5.5,height=4)
 
