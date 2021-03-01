cf4b335e5a57c542cf902c9fd88f6c34f1215333
Han Shaoqing <502466541@qq.com>
2021-03-01 20:34:08 +0800

    Update stat_pos_site.py

--- python/stat_pos_site.py
+++ python/stat_pos_site.py
@@ -19,15 +19,16 @@ def get_AG(site_file):
 				read_length = line[8]
 				base_qual = line[9]
 				read_name = line[11]
+				strand = line[13]
 
 				if int(line[8]) - int(line[7]) == 1 and float(line[9]) > 26 and line[3] == 'A' and line[4] == 'G':
-					content = str(chrID)+'\t'+str(position)+'\t'+ str(ref_base)+'\t'+ str(alt_base)+ '\t'+ str(coverage)
+					content = str(chrID)+'\t'+str(position)+'\t'+ str(ref_base)+'\t'+ str(alt_base)+ '\t'+ str(coverage)+'\t'+strand
 					c[content] +=1
 				else:
 					continue
 	
 	for k,v in c.items():
-		if v >=4:
+		if v >=4 and float(v)/float(k.split('\t')[4]) > 0.1:
 			print(k,"\t",v)
 for i in sys.argv:
 	get_AG(i)
