f1f2be61e79bfbfbcb1e757dd53f95ee01642ba7
Han Shaoqing <502466541@qq.com>
2021-03-01 20:34:36 +0800

    Update trans_neg2pos.py

--- python/trans_neg2pos.py
+++ python/trans_neg2pos.py
@@ -1,6 +1,6 @@
 import sys
 
-def neg(file_neg):
+def neg(file_neg,strand):
 	#row = []
 	with open(file_neg) as f:
 		for line in f:
@@ -28,21 +28,24 @@ def neg(file_neg):
 			elif line[4] == 'N':
 				alt_base = 'N'
 			abs_pos = int(line[8])+1 - int(line[7])
-			print(line[0] +'\t'+ line[1]+ '\t'+line[2]+'\t'+ref_base +'\t'+ alt_base +'\t'+line[5] +'\t'+line[6]+'\t'+str(abs_pos) +'\t'+line[8]+'\t'+line[9] +'\t'+line[10]+'\t'+line[11] +'\t'+line[12])
+			print(line[0] +'\t'+ line[1]+ '\t'+line[2]+'\t'+ref_base +'\t'+ alt_base +'\t'+line[5] +'\t'+line[6]+'\t'+str(abs_pos) +'\t'+line[8]+'\t'+line[9] +'\t'+line[10]+'\t'+line[11] +'\t'+line[12]+'\t'+strand)
 #			print(row)
 
-def pos(file_pos):
+def pos(file_pos,strand):
 	row = []
 	with open(file_pos) as f:
 		for line in f:
 			line = line.strip().split('\t')
 			if len(line) ==1:
 				continue
-			print('\t'.join(line))
+			print('\t'.join(line)+'\t'+strand)
 
 
 file_pos = sys.argv[1]
 file_neg = sys.argv[2]
 
-pos(file_pos)
-neg(file_neg)
+pos(file_pos,'pos')
+neg(file_neg,'neg')
+
+#print(row_pos)
+#print(row_neg)
